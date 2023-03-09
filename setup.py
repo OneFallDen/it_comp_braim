8f8679d92bd6d96ea676684c4b0ed62970b56993
OneFallDen <onefallden@gmail.com>
2023-03-09 19:09:29 +0300

    add setup and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="it_comp_braim",
+    version="1.0.0",
+    url="https://github.com/OneFallDen/it_comp_braim",
+    packages=find_packages()
+)
