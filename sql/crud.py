806578d5d54ff4c7d97b1a9f07a218f912d4e0a1
OneFallDen <onefallden@gmail.com>
2023-03-09 22:19:14 +0300

    get animal by id

--- sql/crud.py
+++ sql/crud.py
@@ -17,3 +17,35 @@ def get_account(account_id: int, db: Session):
         'lastname': result[0].lastname,
         'email': result[0].email
     }
+
+
+"""
+    ANIMAL
+"""
+
+
+def get_animal(animal_id: int, db: Session):
+    result = db.execute(select(models.Animal).where(models.Animal.id == animal_id)).first()
+    result_types = db.execute(select(models.AnimalTypes).where(models.AnimalTypes.animal_id == animal_id)).scalars().all()
+    result_locs = db.execute(select(models.VisitedLocations).where(models.VisitedLocations.animal_id == animal_id)).\
+        scalars().all()
+    animal_types = []
+    animal_locs = []
+    for res in result_locs:
+        animal_locs.append(res.loc_id)
+    for res in result_types:
+        animal_types.append(res.id)
+    return {
+        'id': animal_id,
+        'animalsTypes': animal_types,
+        "weight": result[0].weight,
+        "length": result[0].length,
+        "height": result[0].height,
+        "gender": result[0].gender,
+        "lifeStatus": result[0].lifestatus,
+        "chippingDateTime": result[0].chippingdatetime,
+        "chipperId": result[0].chipperid,
+        "chippingLocationId": result[0].chippinglocationid,
+        "visitedLocations": animal_locs,
+        "deathDateTime": result[0].deathdatetime
+    }
