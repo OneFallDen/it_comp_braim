c71dda5cf305df32475be9f9caebcdbb35bcf61c
OneFallDen <onefallden@gmail.com>
2023-03-09 22:18:12 +0300

    added animal comtroller

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from routers import account_router
+from routers import account_router, animal_router
 
 app = FastAPI()
 
 
 app.include_router(account_router.router)
+app.include_router(animal_router.router)
