09b2c3030e884fe403812db6a357650a447374ed
OneFallDen <onefallden@gmail.com>
2023-03-09 22:57:34 +0300

    router tags fix

--- routers/animal_router.py
+++ routers/animal_router.py
@@ -9,11 +9,11 @@ from controllers.animal_controller import get_anim_info, get_info_type
 router = routing.APIRouter()
 
 
-@router.get('/animal/{animalId}', tags=['account'])
+@router.get('/animal/{animalId}', tags=['animal'])
 async def get_animal_info(animalId: int, db: Session = Depends(get_db)):
     return get_anim_info(animalId, db)
 
 
-@router.get('/animals/types/{typeId}', tags=['account'])
+@router.get('/animals/types/{typeId}', tags=['animal'])
 async def get_type_info(typeId: int, db: Session = Depends(get_db)):
     return get_info_type(typeId, db)
