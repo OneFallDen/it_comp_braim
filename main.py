f3640f57a53b1f4f35854f3ecf3eb180f5cce711
OneFallDen <onefallden@gmail.com>
2023-03-10 14:20:53 +0300

    included reg router

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routers import account_router, animal_router, location_router
+from routers import account_router, animal_router, location_router, reg_router
 
 app = FastAPI()
 
@@ -8,3 +8,4 @@ app = FastAPI()
 app.include_router(account_router.router)
 app.include_router(animal_router.router)
 app.include_router(location_router.router)
+app.include_router(reg_router.router)
