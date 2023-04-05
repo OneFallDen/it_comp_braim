d03ebaf610478223db2a4dd044ab96718ac16a1b
OneFallDen <onefallden@gmail.com>
2023-04-05 18:52:00 +0300

    added string email, role validations

--- controllers/validation_controller.py
+++ controllers/validation_controller.py
@@ -1,7 +1,29 @@
 from typing import List
 from fastapi import HTTPException
+import re
+
+
+pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
 
 
 def check_roles(role: str, roles: List):
     if not role in roles:
         raise HTTPException(status_code=403)
+
+
+def valid_string(string: str):
+    if not string:
+        raise HTTPException(status_code=400)
+    if string.strip() == '':
+        raise HTTPException(status_code=400)
+
+
+def valid_email_pattern(email: str):
+    if re.match(pattern, email) is None:
+        raise HTTPException(status_code=400)
+
+
+def valid_roles(role: str):
+    roles = ["ADMIN", "CHIPPER", "USER"]
+    if not role in roles:
+        raise HTTPException(status_code=400)
